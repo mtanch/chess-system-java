5020a57de66571dd728c40d98c6ece4cca17eae0
mtanch <devluck.2k@gmail.com>
2023-11-07 15:47:13 -0300

    Primeira classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		
-		
+		Position position = new Position(4, 8);
+		System.out.println(position);
 		
 		
 	}
